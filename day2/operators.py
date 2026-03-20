0c43c7dc072400d06c448665971cfa82de4e9c4e
OPIYO OSCAR <opiyooscar414@gmail.com>
2026-03-20 19:36:01 +0300

    Cover operators in python.

---
+++ day2/operators.py
@@ -0,0 +1,13 @@
+# Arithmetic operation(/,+.*,//,%,**)
+a=24
+b=5
+#Addition
+value1=a%b
+print(value1)
+#Comparison operators(==,!=,>=,<=,<,>)
+#Result is always boolean: True or False
+ 
+x=8
+y=8
+result=x!=y
+print(result)
