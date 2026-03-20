ec358cea0f139d988446110508c9b9137ca1542e
OPIYO OSCAR <opiyooscar414@gmail.com>
2026-03-20 18:32:20 +0300

    Cover data types in python.

---
+++ day2/data_types.py
@@ -0,0 +1,18 @@
+#Int 
+age=24
+#string
+name="James"
+#decimal
+PIE=3.14
+is_driver=True
+print(age)
+print(name)
+print(PIE)
+print(is_driver)
+
+#Determining data type
+
+print(type(age))
+print(type(name))
+print(type(PIE))
+print(type(is_driver))
